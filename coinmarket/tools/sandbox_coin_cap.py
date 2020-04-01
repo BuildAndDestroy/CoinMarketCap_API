0093687343bb6c5392e9006216edfe23e89518ae
Mitch O'Donnell <codonnell@purestorage.com>
2020-04-01 00:46:57 -0600

    isort, autopep8 and pylint
    Python standards ran on the tools file

--- coinmarket/tools/sandbox_coin_cap.py
+++ coinmarket/tools/sandbox_coin_cap.py
@@ -36,7 +36,8 @@ def parse_arguments() -> tuple:
     """Give options for user input."""
     url = 'https://sandbox-api.coinmarketcap.com/'
     epilog = f'[*] Tool used for sandbox working with the sandbox environment.\n[*] {url}'
-    parser = argparse.ArgumentParser(epilog=epilog, formatter_class=argparse.RawTextHelpFormatter)
+    parser = argparse.ArgumentParser(
+        epilog=epilog, formatter_class=argparse.RawTextHelpFormatter)
     parser.add_argument('-c', '--coins', action='append', type=lambda coin_and_portfolio_value: coin_and_portfolio_value.lower().split('='), nargs='*', dest='coins',
                         help='Provide coin names with/without the amount of coin in your wallet.\nExample: \n-c Bitcoin=10 eos ethereum=5\n-c bitcoin')
     parser.add_argument('-f', '--format', action='store_true',
@@ -54,46 +55,77 @@ def parse_arguments() -> tuple:
 
     subparsers = parser.add_subparsers(help='commands', dest='command')
 
-    cryptocurrency_parser = subparsers.add_parser('cryptocurrency', help='Use the cryptocurrency endpoint.')
-    cryptocurrency_parser.add_argument('--map', action='store_true', help='Use the cryptocurrency/map API call.')
-    cryptocurrency_parser.add_argument('--info', action='store_true', help='Use the cryptocurrency/info API call.')
-    cryptocurrency_parser.add_argument('--listings-latest', action='store_true', help='Use the cryptocurrency/listings/latest API call.')
-    cryptocurrency_parser.add_argument('--listings-historical', action='store_true', help='Use the cryptocurrency/listings/historical API call.')
-    cryptocurrency_parser.add_argument('--quotes-latest', action='store_true', help='Use the cryptocurrency/quotes/latest API call.')
-    cryptocurrency_parser.add_argument('--quotes-historical', action='store_true', help='Use the cryptocurrency/quotes/historical API call.')
-    cryptocurrency_parser.add_argument('--market-pairs-latest', action='store_true', help='Use the cryptocurrency/market/pairs/latest API call.')
-    cryptocurrency_parser.add_argument('--ohlcv-latest', action='store_true', help='Use the cryptocurrency/ohlcv/latest API call.')
-    cryptocurrency_parser.add_argument('--ohlcv-historical', action='store_true', help='Use the cryptocurrency/ohlcv/historical API call.')
-    cryptocurrency_parser.add_argument('--price-performance-stats-latest', action='store_true', help='Use the cryptocurrency/price/performance/stats/latest API call.')
+    cryptocurrency_parser = subparsers.add_parser(
+        'cryptocurrency', help='Use the cryptocurrency endpoint.')
+    cryptocurrency_parser.add_argument(
+        '--map', action='store_true', help='Use the cryptocurrency/map API call.')
+    cryptocurrency_parser.add_argument(
+        '--info', action='store_true', help='Use the cryptocurrency/info API call.')
+    cryptocurrency_parser.add_argument(
+        '--listings-latest', action='store_true', help='Use the cryptocurrency/listings/latest API call.')
+    cryptocurrency_parser.add_argument(
+        '--listings-historical', action='store_true', help='Use the cryptocurrency/listings/historical API call.')
+    cryptocurrency_parser.add_argument(
+        '--quotes-latest', action='store_true', help='Use the cryptocurrency/quotes/latest API call.')
+    cryptocurrency_parser.add_argument(
+        '--quotes-historical', action='store_true', help='Use the cryptocurrency/quotes/historical API call.')
+    cryptocurrency_parser.add_argument(
+        '--market-pairs-latest', action='store_true', help='Use the cryptocurrency/market/pairs/latest API call.')
+    cryptocurrency_parser.add_argument(
+        '--ohlcv-latest', action='store_true', help='Use the cryptocurrency/ohlcv/latest API call.')
+    cryptocurrency_parser.add_argument(
+        '--ohlcv-historical', action='store_true', help='Use the cryptocurrency/ohlcv/historical API call.')
+    cryptocurrency_parser.add_argument('--price-performance-stats-latest', action='store_true',
+                                       help='Use the cryptocurrency/price/performance/stats/latest API call.')
 
-    exchange_parser = subparsers.add_parser('exchange', help='Use the exchange endpoint.')
-    exchange_parser.add_argument('--map', action='store_true', help='Use the /v1/exchange/map API call.')
-    exchange_parser.add_argument('--info', action='store_true', help='Use the /v1/exchange/info API call.')
-    exchange_parser.add_argument('--listings-latest', action='store_true', help='Use the /v1/exchange/listings/latest API call.')
-    exchange_parser.add_argument('--listings-historical', action='store_true', help='Use the /v1/exchange/listings/historical API call.')
-    exchange_parser.add_argument('--quotes-latest', action='store_true', help='Use the /v1/exchange/quotes/latest API call.')
-    exchange_parser.add_argument('--quotes-historical', action='store_true', help='Use the /v1/exchange/quotes/historical API call.')
-    exchange_parser.add_argument('--market-pairs-latest', action='store_true', help='Use the /v1/exchange/market-pairs/latest API call.')
+    exchange_parser = subparsers.add_parser(
+        'exchange', help='Use the exchange endpoint.')
+    exchange_parser.add_argument(
+        '--map', action='store_true', help='Use the /v1/exchange/map API call.')
+    exchange_parser.add_argument(
+        '--info', action='store_true', help='Use the /v1/exchange/info API call.')
+    exchange_parser.add_argument('--listings-latest', action='store_true',
+                                 help='Use the /v1/exchange/listings/latest API call.')
+    exchange_parser.add_argument('--listings-historical', action='store_true',
+                                 help='Use the /v1/exchange/listings/historical API call.')
+    exchange_parser.add_argument(
+        '--quotes-latest', action='store_true', help='Use the /v1/exchange/quotes/latest API call.')
+    exchange_parser.add_argument('--quotes-historical', action='store_true',
+                                 help='Use the /v1/exchange/quotes/historical API call.')
+    exchange_parser.add_argument('--market-pairs-latest', action='store_true',
+                                 help='Use the /v1/exchange/market-pairs/latest API call.')
 
-    global_metrics_parser = subparsers.add_parser('global-metrics', help='Use the global-metrics endpoint.')
-    global_metrics_parser.add_argument('--latest', action='store_true', help='Use the /v1/global-metrics/quotes/latest API call.')
-    global_metrics_parser.add_argument('--historical', action='store_true', help='Use the /v1/global-metrics/quotes/historical API call.')
+    global_metrics_parser = subparsers.add_parser(
+        'global-metrics', help='Use the global-metrics endpoint.')
+    global_metrics_parser.add_argument(
+        '--latest', action='store_true', help='Use the /v1/global-metrics/quotes/latest API call.')
+    global_metrics_parser.add_argument(
+        '--historical', action='store_true', help='Use the /v1/global-metrics/quotes/historical API call.')
 
-    tools_parser = subparsers.add_parser('tools', help='Use the tools endpoint.')
-    tools_parser.add_argument('--price-conversion', action='store_true', help='Use the /v1/tools/price-conversion API call.')
+    tools_parser = subparsers.add_parser(
+        'tools', help='Use the tools endpoint.')
+    tools_parser.add_argument('--price-conversion', action='store_true',
+                              help='Use the /v1/tools/price-conversion API call.')
 
-    blockchain_parser = subparsers.add_parser('blockchain', help='Use the blockchain endpoint.')
-    blockchain_parser.add_argument('--latest', action='store_true', help='Use the /v1/blockchain/statistics/latest API call.')
+    blockchain_parser = subparsers.add_parser(
+        'blockchain', help='Use the blockchain endpoint.')
+    blockchain_parser.add_argument(
+        '--latest', action='store_true', help='Use the /v1/blockchain/statistics/latest API call.')
 
     fiat_parser = subparsers.add_parser('fiat', help='Use the fiat endpoint.')
-    fiat_parser.add_argument('--map', action='store_true', help='Use the /v1/fiat/map API call.')
+    fiat_parser.add_argument('--map', action='store_true',
+                             help='Use the /v1/fiat/map API call.')
 
-    partners_parser = subparsers.add_parser('partners', help='Use the partners endpoint.')
-    partners_parser.add_argument('--listings-latest', action='store_true', help='Use the /v1/partners/flipside-crypto/fcas/listings/latest API call.')
-    partners_parser.add_argument('--quotes-latest', action='store_true', help='Use the /v1/partners/flipside-crypto/fcas/quotes/latest API call.')
+    partners_parser = subparsers.add_parser(
+        'partners', help='Use the partners endpoint.')
+    partners_parser.add_argument('--listings-latest', action='store_true',
+                                 help='Use the /v1/partners/flipside-crypto/fcas/listings/latest API call.')
+    partners_parser.add_argument('--quotes-latest', action='store_true',
+                                 help='Use the /v1/partners/flipside-crypto/fcas/quotes/latest API call.')
 
     key_parser = subparsers.add_parser('key', help='Use the key endpoint.')
-    key_parser.add_argument('--info', action='store_true', help='Use the /v1/key/info API call.')
+    key_parser.add_argument('--info', action='store_true',
+                            help='Use the /v1/key/info API call.')
 
     args = parser.parse_args()
     return args
@@ -105,7 +137,7 @@ def main() -> None:
     If no arguments present, return all dictionaries.
     If arguments, return dictionaries with/without formatting.
     """
-    print('Coming soon!')
+    print(f'[*] ****Coming soon!****\n')
     args = parse_arguments()
     print(args)
 
